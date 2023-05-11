5103416513ba086dff40c90f2454b9a82ec7848e
justosky <justosky2011@yahoo.com>
2023-05-11 13:50:54 +0000

    A C header file

--- shell.h
+++ shell.h
@@ -1,6 +1,13 @@
 #ifndef shell_h
 #define shell_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#define BUFFER_SIZE 1024
 
 int stringcmp(char *StringA, char * StringB);
+char *get_user_input();
 
 #endif /* End of shell_h */
