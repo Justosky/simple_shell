41b3142234a7aec33249d2d0944c6bc27c9a46df
justosky <justosky2011@yahoo.com>
2023-05-12 08:52:03 +0000

    Computes the length of a string

---
+++ stringlen.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+*stringlen - stringlen function computes the length of a string.
+*It takes a character pointer as an argument. It initializes an
+*integer variable count. It checks if string is a NULL pointer and
+*prints an error message and return (count - 1) which is same as -1
+*Else it checks if String at an index of count contains the null character
+*it increments count and the loop continues till String at an index of count
+*becomes null. When the loop breaks it returns the current value of count
+*which is the total number of characters within the string excluding the
+*null byte.
+*@String: The string that it's length is to be calculated.
+*Return: The function returns count on success or (count - 1) on failure.
+*/
+
+int stringlen(char *String)
+{
+	int count = 0;
+
+	if (String == NULL)
+	{
+		perror("Error encountered while calculating the lenght of an empty string");
+		return (count - 1);
+	}
+	else
+	{
+		for (count = 0; String[count] != '\0'; count++)
+		{	}
+	return (count);
+	}
+}
+
