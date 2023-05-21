9509329a35ac7094119e54a407fa66a1829b430c
justosky <justosky2011@yahoo.com>
2023-05-21 23:38:01 +0000

    Updated this function to be betty compliant

--- get_path.c
+++ get_path.c
@@ -2,17 +2,17 @@
 
 /**
 **get_path - This function gets the value of PATH environmental variable
-*as it is on a user computer. It exlude this string "PATH=" from 
-the value of path. The function initializes a character pointer
+*as it is on a user computer. It exlude this string "PATH=" from
+*the value of path. The function initializes a character pointer
 *path which we will use to store the value of PATH. It initializes
 *an interger variable count. it checks if environmental variable
 *double pointer is holding a valid memomory address. If it is not
 *we print an error message and return NULL. Else we transverse the
 *envp double pointer looking for an environmental variable which
 *the first five characters matches the string "PATH=". When that
-*environmental variable is found we start to copy the value 
+*environmental variable is found we start to copy the value
 *of that environmental variable from the fifth character to another
-*pointer path. The pointer path which now holds the value of the 
+*pointer path. The pointer path which now holds the value of the
 *environmental variable PATH is returned within function.
 *@envp: A double pointer which is the third argument of the main function.
 *It holds the name of all environmental variables and it's value as it is
@@ -34,12 +34,12 @@ char *get_path(char **envp)
 	}
 	else
 	{
-		for(count = 0; envp[count] != NULL; count++)
+		for (count = 0; envp[count] != NULL; count++)
 		{
 			if ((stringcmp(envp[count], "PATH=", 4)) == 0)
 			{
 				path = envp[count] + 5;
-				return (path); 
+				return (path);
 			}
 		}
 	}
